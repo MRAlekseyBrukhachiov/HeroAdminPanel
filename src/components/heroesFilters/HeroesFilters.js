c020f386f349f5e5ec01e1196ee71d2e645b0aa6
Aleksey <aleksey.bryukhachiov@yandex.ru>
2023-10-04 22:02:50 +0300

    redux-thunk

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import classNames from "classnames";
 
-import { filtersFetching, filtersFetched, filtersFetchingError, activeFilteredChanged } from "../../actions";
+import { fetchFilters, activeFilteredChanged } from "../../actions";
 import { useHttp } from "../../hooks/http.hook";
 
 import Spinner from "../spinner/Spinner";
@@ -21,11 +21,7 @@ const HeroesFilters = () => {
     const {request} = useHttp();
 
     useEffect(() => {
-        dispatch(filtersFetching());
-        request("http://localhost:3001/filters")
-            .then(data => dispatch(filtersFetched(data)))
-            .catch(() => dispatch(filtersFetchingError()))
-
+        dispatch(fetchFilters(request));
         // eslint-disable-next-line
     },[]);
 
